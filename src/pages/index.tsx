709e840b4618e00ecfc026c06db1f74e9b3c6167
Ramon Seugling <ramonseugling@gmail.com>
2022-04-11 22:12:05 -0300

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +25,27 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <div className={styles.posts}>
+        <img src="logo.svg" alt="logo" />
+
+        <a href="posts/oi">
+          <h2>Como utilizar Hooks</h2>
+          <p>Pensando em sicronização em vez de ciclos de vida.</p>
+          <time>
+            <FiCalendar /> 15 Mar 2021
+          </time>
+          <span>
+            <FiUser />
+            Joseph Oliveira
+          </span>
+        </a>
+      </div>
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
