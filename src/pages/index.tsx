c3030f08824707594c57bcf72b2ce2c8acf5f786
Ramon Seugling <ramonseugling@gmail.com>
2022-04-12 21:44:55 -0300

    Added reading of posts from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { GetStaticProps } from 'next';
 
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import Link from 'next/link';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -25,31 +28,63 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
   return (
     <main className={styles.container}>
       <div className={styles.posts}>
         <img src="logo.svg" alt="logo" />
+        {postsPagination.results.map(post => (
+          <Link href="posts/oi">
+            <a href="posts/oi">
+              <h2>{post.data.title}</h2>
+              <p>Pensando em sicronização em vez de ciclos de vida.</p>
+              <div className={styles.dateAndWriterInfo}>
+                <time>
+                  <FiCalendar /> 15 Mar 2021
+                </time>
+                <span>
+                  <FiUser />
+                  Joseph Oliveira
+                </span>
+              </div>
+            </a>
+          </Link>
+        ))}
 
-        <a href="posts/oi">
-          <h2>Como utilizar Hooks</h2>
-          <p>Pensando em sicronização em vez de ciclos de vida.</p>
-          <time>
-            <FiCalendar /> 15 Mar 2021
-          </time>
-          <span>
-            <FiUser />
-            Joseph Oliveira
-          </span>
-        </a>
+        <button type="button">Carregar mais posts</button>
       </div>
     </main>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.content'],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      subtitle: RichText.asText(post.data.subtitle),
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
